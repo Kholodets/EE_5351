3d7b3d768576a23f4678275e9ba6c014d837174c
Alexis <alexis@kholo.moe>
2025-10-27 16:20:23 -0500

    strange correctness issues

--- MP4-2/scan_largearray_kernel.cu
+++ MP4-2/scan_largearray_kernel.cu
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 extern const int NUM_BANKS = 16;
 extern const int LOG_NUM_BANKS = 4;
 extern const int TILE_SIZE = 1024;
@@ -6,6 +8,11 @@ extern const int BLOCK_SIZE = (TILE_SIZE / 2);
 
 
 
+__global__ void add_all(unsigned int *data, int n, unsigned int *aux);
+__global__ void scan_kernel(unsigned int *in_data, unsigned int *out_data, int n, unsigned int *aux);
+void prescanArray(unsigned int *outArray, unsigned int *inArray, unsigned int* blockSums, int numElements);
+unsigned int* preallocBlockSums(int num_elements);
+void deallocBlockSums(unsigned int* bs_d);
 // Device Functions
 
 
@@ -15,10 +22,13 @@ extern const int BLOCK_SIZE = (TILE_SIZE / 2);
 
 // Kernel Functions
 
-__global void add_all(unsigned int *data, int n, unsigned int *aux)
+__global__ void add_all(unsigned int *data, int n, unsigned int *aux)
 {
-	__shared__ unsigned int b = aux[blockIdx.x];
+	__shared__ unsigned int b;
 	unsigned int t = threadIdx.x + blockDim.x * blockIdx.x;
+	if (t == 0) {
+		b = aux[blockIdx.x];
+	}
 
 	if (t < n) {
 		int a = data[t];
@@ -31,7 +41,7 @@ __global void add_all(unsigned int *data, int n, unsigned int *aux)
 __global__ void scan_kernel(unsigned int *in_data, unsigned int *out_data, int n, unsigned int *aux)
 {
 	//setup + readin
-	__shared__ scan_array[BLOCK_SIZE * 2];
+	__shared__ unsigned int scan_array[BLOCK_SIZE * 2];
 
 	unsigned int t = threadIdx.x;
 	unsigned int start = 2 * blockDim.x * blockIdx.x;
@@ -46,12 +56,38 @@ __global__ void scan_kernel(unsigned int *in_data, unsigned int *out_data, int n
 			scan_array[index] += scan_array[index - stride];
 		}
 		stride <<= 1;
+		
 		__syncthreads();
+
+/*		if (t == 0) {
+			for (int k = 0; k < BLOCK_SIZE * 2; ++k) {
+				printf("%d ", scan_array[k]);
+			}
+			printf("\n");
+		}
+*/
 	}
+
+	__syncthreads();
 	
+	if (t == 0) {
+			for (int k = 0; k < BLOCK_SIZE * 2; ++k) {
+				printf("%d ", scan_array[k]);
+			}
+			printf("\n");
+	}
+
+
 	//distribution step
 	stride = BLOCK_SIZE / 2;
 	while (stride) {
+
+		/*if (t == 0) {
+			for (int k = 0; k < BLOCK_SIZE * 2; ++k) {
+				printf("%d ", scan_array[k]);
+			}
+			printf("\n");
+		}*/		
 		int index = (threadIdx.x + 1) * stride * 2 - 1;
 		if ((index + stride) < 2 * BLOCK_SIZE) {
 			scan_array[index + stride] += scan_array[index];
@@ -84,20 +120,20 @@ __global__ void scan_kernel(unsigned int *in_data, unsigned int *out_data, int n
 // functions in this file, and then call them from here.
 // Note that the code has been modified to ensure numElements is a multiple 
 // of TILE_SIZE
-void prescanArray(unsigned int *outArray, unsigned int *inArray, unsigned int* blockSums, int numElements);
 
 void prescanArray(unsigned int *outArray, unsigned int *inArray, unsigned int* blockSums, int numElements)
 {   //all pointers are to device memory regions
     int tiles = (numElements + TILE_SIZE - 1) / TILE_SIZE;
+    printf("reducing from %d to %d\n", numElements, tiles);
     scan_kernel<<<tiles, BLOCK_SIZE>>>(inArray, outArray, numElements, blockSums);
 
     if (tiles > 1) {
 	    unsigned int *nextBlocks = preallocBlockSums(tiles);
 	    prescanArray(blockSums, blockSums, nextBlocks, tiles);
 	    deallocBlockSums(nextBlocks);
+	    add_all<<<tiles - 1, BLOCK_SIZE>>>(outArray + TILE_SIZE, numElements - TILE_SIZE, blockSums);
     }
 
-    add_all<<<tiles - 1, BLOCK_SIZE>>>(outArray + TILE_SIZE, numElements - TILE_SIZE, blockSums);
 
 }
 // **===-----------------------------------------------------------===**
